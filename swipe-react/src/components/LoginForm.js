4e2551c578d82a028ad1d987167e59175c0dfb14
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-07 15:08:40 -0600

    stubbed login.js

---
+++ swipe-react/src/components/LoginForm.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const LoginForm = () => {
+  return (
+    <form>
+        <label>Name: </label>
+        <input id="name" type="text" required/>
+    </form>
+  )
+}
+
+export default LoginForm
