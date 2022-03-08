c8d5ddd966b65ea587ccebf7c0652510e8d0554b
Tony Shifflett <charles.l.shifflett@gmail.com>
2022-03-07 18:55:22 -0600

    added password field to login form

--- swipe-react/src/components/LoginForm.js
+++ swipe-react/src/components/LoginForm.js
@@ -5,6 +5,8 @@ const LoginForm = () => {
     <form>
         <label>Name: </label>
         <input id="name" type="text" required/>
+        <label>Password: </label>
+        <input id="password" type="password" required/>
     </form>
   )
 }
